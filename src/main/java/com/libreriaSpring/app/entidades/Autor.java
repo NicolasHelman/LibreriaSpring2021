0d70faae41471426c55df1a0ddca9a4f41e92344
Nicolas Helman <nicolashelman11@gmail.com>
2021-09-29 19:54:00 -0300

    Actualizando cambios en entidades

--- src/main/java/com/libreriaSpring/app/entidades/Autor.java
+++ src/main/java/com/libreriaSpring/app/entidades/Autor.java
@@ -2,16 +2,17 @@ package com.libreriaSpring.app.entidades;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "autor")
 public class Autor {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private String nombre;
     private Boolean alta;
--- src/main/java/com/libreriaSpring/app/entidades/Libro.java
+++ src/main/java/com/libreriaSpring/app/entidades/Libro.java
@@ -2,17 +2,18 @@ package com.libreriaSpring.app.entidades;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table(name = "libro")
 public class Libro {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
     private Long isbn;
     private String titulo;
