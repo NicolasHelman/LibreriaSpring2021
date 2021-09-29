51c0484278425f4ca9650d93803a5684765dc32c
Nicolas Helman <nicolashelman11@gmail.com>
2021-09-29 18:30:50 -0300

    Comienzo del Proyecto Libreria

---
+++ src/test/java/com/libreriaSpring/app/LibreriaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreriaSpring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
